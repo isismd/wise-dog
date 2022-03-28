dce8dbabf20da182a1f0df200de6c887e347aed4
Isis Daron <daronisism@gmail.com>
2022-03-28 15:41:10 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 const btn = document.getElementById('another_btn');
 const text = document.getElementById('advice');
 const dog = document.getElementById('picture');
+const adjective = document.getElementById('author');
 
 document.addEventListener("DOMContentLoaded", getRandomAdvice)
 document.addEventListener("DOMContentLoaded", getRandomDog)
+document.addEventListener("DOMContentLoaded", getRandomAdjective)
 btn.addEventListener('click', getRandomDog);
 btn.addEventListener('click', getRandomAdvice);
+btn.addEventListener('click', getRandomAdjective);
 
 function getRandomAdvice() {
 	fetch('https://api.adviceslip.com/advice')
@@ -26,4 +29,28 @@ function getRandomDog() {
 				dog.innerHTML = `<img src=${data.url} alt="dog picture" height="400" width="400"  />`;
 			}
 		});
+}
+
+function getRandomAdjective(){
+     
+      
+	var theAdjective= [
+	  '- Random Dog',
+	  '- Mr. Dog',
+	  '- Happy Dog',
+	  '- Confused Dog',
+	  '- Super Dog',
+	  '- Silly Dog',
+	  '- Nice Dog',
+	  '- Funny Dog',
+	  '- Tired Dog',
+	  '- Important Dog',
+	  '- Cool Dog',
+	  '- Suspicious Dog',
+	  '- Mysterious Dog',
+	];
+
+	
+	var wordNum = Math.floor(Math.random() * theAdjective.length);
+	adjective.innerHTML = theAdjective[Math.floor(Math.random() * theAdjective.length)];
 }
